21b36751c78b0ef4d81c4d8241dd906d4b1b789c
ivan.heibi <ivanhb.ita@gmail.com>
2022-06-27 15:17:52 +0200

    index free search

--- static/js/search-conf-index.js
+++ static/js/search-conf-index.js
@@ -123,12 +123,12 @@ var search_conf = {
             "spinner": true,
             "title":"Searching in the OpenCitations Indexes ...",
             "subtitle":"Be patient - this search might take several seconds!",
-            "abort":{"title":"Abort Search","href_link":"search"}
+            "abort":{"title":"Abort Search","href_link":"/index/search"}
           },
 
    "timeout":{
             "value": 9000,
-            "link": "search.html"
+            "link": "/index/search"
           }
 
   }
