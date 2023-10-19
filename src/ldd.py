cf0ebad9d3f9fc93c4fe3ae58295b2be1d41f08d
ivan.heibi <ivanhb.ita@gmail.com>
2023-10-19 16:50:59 +0200

    sources content neg handler

--- src/ldd.py
+++ src/ldd.py
@@ -251,7 +251,13 @@ class LinkedDataDirector(object):
                     #res = self.tp.query("CONSTRUCT {?s ?p ?o} "
                     #                    "WHERE { <%s> ?p ?o . BIND(<%s> as ?s) }" %
                     #                    (resource_url, resource_url))
-                    sparql_query = "CONSTRUCT {?s ?p ?o} WHERE { <"+resource_url+"> ?p ?o . BIND (<"+resource_url+"> as ?s) }"
+                    _a_source_t = ""
+                    for _a_source in ["index/coci/","index/doci/","index/poci/","index/oroci/","index/croci/"]:
+                        if _a_source in resource_url:
+                            resource_url = resource_url.replace(_a_source,"index/")
+                            _a_source_t = "<"+resource_url+"> <http://www.w3.org/ns/prov#atLocation> <https://w3id.org/oc/"+_a_source+"> ."
+
+                    sparql_query = "CONSTRUCT {?s ?p ?o} WHERE { <"+resource_url+"> ?p ?o . "+_a_source_t+" BIND (<"+resource_url+"> as ?s) }"
                     res = requests.get(self.tp, params={"query": sparql_query}, headers={"Accept":"text/turtle"})
 
                     if res.status_code == 200:
