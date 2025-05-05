b05f9b3052c0d20758fabffe334e66e3d2f7d05e
Ivan Heibi <ivanhb.ita@gmail.com>
2025-05-05 23:33:22 +0200

    fix search index v2

--- static/js/search-conf-index-v2.js
+++ static/js/search-conf-index-v2.js
@@ -1,5 +1,5 @@
 var search_conf = {
-"sparql_endpoint": "https://opencitations.net/index/sparql",
+"sparql_endpoint": "https://test.opencitations.net/meta/sparql",
 "prefixes": [
     {"prefix":"cito","iri":"http://purl.org/spar/cito/"},
     {"prefix":"literal","iri":"http://www.essepuntato.it/2010/06/literalreification/"},
@@ -10,6 +10,24 @@ var search_conf = {
   ],
 
 "rules":  [
+    /*{
+      "name":"citedomid",
+      "label": "Citations of a bibliographic resource (OMID)",
+      "placeholder": "e.g. br/0612058700",
+      "advanced": true,
+      "freetext": true,
+      "category": "citation",
+      "regex":"(.+)",
+      "query": [`
+            {
+              SERVICE <https://test.opencitations.net/index/sparql> {
+                    BIND(<https://w3id.org/oc/meta/[[VAR]]> as ?cited) .
+                    ?oci cito:hasCitedEntity ?cited .
+                    ?oci cito:hasCitingEntity ?citing .
+              }
+            }`
+      ]
+    },*/
     {
       "name":"citingdoi",
       "label": "References of a bibliographic resource (DOI, PMID, OMID)",
@@ -21,12 +39,13 @@ var search_conf = {
       "regex":"(.+)",
       "query": [`
             {
-              SERVICE <https://test.opencitations.net/meta/sparql> {
-                ?citing datacite:hasIdentifier ?identifier .
                 ?identifier literal:hasLiteralValue "[[VAR]]" .
-              }
-              ?oci cito:hasCitingEntity ?citing .
-              ?oci cito:hasCitedEntity ?cited .
+                ?citing datacite:hasIdentifier ?identifier .
+                SERVICE <https://opencitations.net/index/sparql> {
+                      ?oci a cito:Citation .
+                      ?oci cito:hasCitingEntity ?citing .
+                      ?oci cito:hasCitedEntity ?cited .
+                }
             }`
       ]
     },
@@ -41,12 +60,13 @@ var search_conf = {
       "regex":"(.+)",
       "query": [`
             {
-              SERVICE <https://test.opencitations.net/meta/sparql> {
-                ?cited datacite:hasIdentifier ?identifier .
                 ?identifier literal:hasLiteralValue "[[VAR]]" .
-              }
-              ?oci cito:hasCitedEntity ?cited .
-              ?oci cito:hasCitingEntity ?citing .
+                ?cited datacite:hasIdentifier ?identifier .
+                SERVICE <https://opencitations.net/index/sparql> {
+                      ?oci a cito:Citation .
+                      ?oci cito:hasCitedEntity ?cited .
+                      ?oci cito:hasCitingEntity ?citing .
+                }
             }`
       ]
     },
@@ -60,9 +80,11 @@ var search_conf = {
       "regex":"(\\d{1,}-\\d{1,})",
       "query": [`
         {
-          BIND(<https://w3id.org/oc/index/ci/[[VAR]]> as ?oci) .
+        BIND(<https://w3id.org/oc/index/ci/[[VAR]]> as ?oci) .
+        SERVICE <https://opencitations.net/index/sparql> {
           ?oci cito:hasCitingEntity ?citing .
           ?oci cito:hasCitedEntity ?cited .
+        }
         }
         `
       ]
@@ -91,7 +113,7 @@ var search_conf = {
         "cited_ref": {"name": "meta_call_to_get_ref", "param": {"fields":["cited"]}, "async": true}
       },
       "extra_elems":[
-        {"elem_type": "a","elem_value": "Back to search" ,"elem_class": "btn btn-primary left" ,"elem_innerhtml": "Show the search interface", "others": {"href": "/index/search"}}
+        {"elem_type": "a","elem_value": "Back to search" ,"elem_class": "btn btn-primary left" ,"elem_innerhtml": "Show the search interface", "others": {"href": "/"}}
       ]
     }
   ],
@@ -108,17 +130,17 @@ var search_conf = {
             "spinner": true,
             "title":"Searching in the OpenCitations Indexes ...",
             "subtitle":"Be patient - this search might take several seconds!",
-            "abort":{"title":"Abort Search","href_link":"/index/search"}
+            "abort":{"title":"Abort Search","href_link":"/"}
           },
 
    "timeout":{
             "value": 9000,
-            "link": "/index/search"
+            "link": "/"
           }
 
   }
 
-console.log(search_conf);
+//console.log(search_conf);
 
 
 var heuristics = (function () {
